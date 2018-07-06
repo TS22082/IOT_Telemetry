590b3924b37af8869f7e476fd658fb2d59eecd3a
TS22082 <ts22082@gmail.com>
2018-07-05 21:38:15 -0700

    first commit for tutorial

---
+++ NodeIOT/NodeIOT.ino
@@ -0,0 +1,63 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+SoftwareSerial port1(D2,D3); //RX,TX
+SoftwareSerial port2(D6,D7); //RX,TX
+
+int period = 1000;
+unsigned long time_now = 0;
+
+int relay_1 = 16;
+int relay_2 = 5;
+
+#define FIREBASE_HOST "---fill in your own info here--"
+#define FIREBASE_AUTH "--this is the secret key---"
+#define WIFI_SSID "--fill in your own info here---"
+#define WIFI_PASSWORD "---fill in your own info here--"
+
+void setup(){
+  Serial.begin(9600);
+  port1.begin(4800);
+  port2.begin(4800);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);  
+  }
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop(){ 
+  port1.listen();
+  delay(200);
+  while(port1.available()>0){
+    float val = port1.parseFloat();
+    if(port1.read()== '\n'){
+      Serial.print("CON: ");
+      Serial.println(val);
+      Firebase.setFloat("CON", val);
+      break;
+    }
+  }
+  
+  port2.listen();
+  delay(200);
+  while(port2.available()>0){
+    float val = port2.parseFloat();
+    if(port2.read()== '\n'){
+      Serial.print("PH: ");
+      Serial.println(val);
+      Firebase.setFloat("PH", val);
+      break;
+    }
+  }
+  
+  time_now = millis();
+  while(millis() < time_now + period){}
+}
---
+++ UnoSensor/UnoSensor.ino
@@ -0,0 +1,93 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#define coAddress 100
+#define phAddress 99
+
+byte code=0;                
+char data[20];            
+byte in_char=0;                  
+byte i=0;                      
+int time_=2000;                 
+float RTD_float;   
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+SoftwareSerial port1(11,10); //RX,TX
+SoftwareSerial port2(13,12);
+
+int period = 1000;
+unsigned long time_now = 0;
+
+void setup() {  
+  Serial.begin(9600);
+  port1.begin(4800);
+  port2.begin(4800);
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+ //for conductivity
+ Wire.beginTransmission(coAddress);
+ Wire.write('r');
+ Wire.endTransmission();
+ delay(time_);
+ Wire.requestFrom(coAddress, 20, 1);
+ 
+ code = Wire.read();
+ while(Wire.available()){        
+  in_char = Wire.read();        
+  data[i]= in_char;             
+  i+=1;                            
+  if(in_char==0){                 
+    i=0;                        
+    Wire.endTransmission();     
+    break;                      
+    }
+  }
+  lcd.setCursor(0,0);
+  lcd.print("CN: ");
+  lcd.setCursor(6,0);
+  lcd.print(data);
+  port1.print(data);
+  port1.println("\n");
+  delay(30);
+  
+  //for PH
+  Wire.beginTransmission(phAddress);
+  Wire.write('r');
+  Wire.endTransmission();
+  delay(time_);
+  Wire.requestFrom(phAddress, 20, 1);
+  code = Wire.read();
+  while(Wire.available()){        
+    in_char = Wire.read();        
+    data[i]= in_char;             
+    i+=1;                            
+    if(in_char==0){                 
+      i=0;                        
+      Wire.endTransmission();     
+      break;                      
+    }
+  }
+  lcd.setCursor(0,1);
+  lcd.print("PH: ");
+  lcd.setCursor(6,1);
+  lcd.print(data);
+  port2.print(data);
+  port2.println("\n");
+  delay(30);
+
+  port1.listen();
+  delay(200);
+  while(port1.available()>0){
+    float val = port1.parseFloat();
+    if(port1.read()== '\n'){
+      Serial.print("from port 1: ");
+      Serial.println(val);
+      break;
+    }
+  }
+ }
